04b36506c4dd6acd9245f1a2831be07411e4a2a5
HaiDang <lthdang@ninepoints.vn>
2024-01-26 09:23:29 +0700

    Django: firsts commit

---
+++ home/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+# Create your views here.
+
+def index(request):
+    return render(request, 'pages/home.html')
+def contact(request):
+    return render(request, 'pages/contact.html')
+def error(request):
+    return render(request, 'pages/error.html')
